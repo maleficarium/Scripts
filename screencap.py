3a37a33f86a4564205553b32e7778280a6a6b30f
maleficarium <maleficarium@gmail.com>
2015-03-07 07:25:58 +0200

    Variables cleanup

--- screencap.py
+++ screencap.py
@@ -14,27 +14,25 @@ path = 'M:\!!!Torrents'
 animeid = None
 animelink = ''
 while animeid == None:
-	uid = ''
-	uid = raw_input('Enter Episode: ')
-	uid = uid.split()
-	sid = ''
-	for x in uid:
-		sid = sid+'*'+x
-	sid = sid+'*[*'
-	animelink = glob.glob(os.path.join(path, sid))
+	animecode = ''
+	animecode = raw_input('Enter Episode: ')
+	animecode = animecode.split()
+	regID = ''
+	for x in animecode:
+		regID = regID+'*'+x
+	regID = regID+'*[*'
+	animelink = glob.glob(os.path.join(path, regID))
 	print animelink
 	check = raw_input('Return: Confirm, r: Retry: ')
 	if check == '' and len(animelink) == 1:
-		animeid = sid
-	else:
+		animeid = regID
+	elif check != 'r':
 		print "Please be more specific."
 
 position = raw_input('Time MM:SS: ')
 duration = raw_input('Duration S: ')
 
-animeid = uid[0] + '_'
-
-ffmpegcommand = "ffmpeg -ss " + position + " -i \"" + animelink[0] + "\" -t "  + duration + " -r 24 e:\\autoscreenshots\\" + animeid + randomword(5) + "_%04d.png"
+ffmpegcommand = "ffmpeg -ss " + position + " -i \"" + animelink[0] + "\" -t "  + duration + " -r 24 e:\\autoscreenshots\\" + animecode[0] + "_" + randomword(5) + "_%04d.png"
 print ffmpegcommand
 
 os.system(ffmpegcommand)
